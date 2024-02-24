e1904ac249d5c4d3a733e36299719f5230194d93
PIERO CABALLERO DK <161093675+pierofrxxo10@users.noreply.github.com>
2024-02-24 11:45:39 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from openai import openai
+
+
+client = OpenAI(api_key =st.secrets.OpenAI.openai_api_key)
+
+if "messages" not in st.session_state:
+  st.session_state["messages"] = [
+    {"role":"system","content":"Piensa como un ingeniero de Software"}
+  ]
+
+def comunicate():
+  messages = st.session_state["messages"]
+  user_message = {"role":"user","content":st.session_state["user input"]}
+  messages.append(user_message)
+
+  response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages = messages
+  )
+
+  bot_message = response.choices[0].messages
+  messages.append(bot_message)
+
+  st.session_state("user_input") = ""
+
+  st.title("Desarrolador AI")
+  st.write("Utilizando la API chatGPT, este chatbot ofrece capacidades conversacionales avanzadas.")
+
+  user_input = st.text_input("Por favor ingrese unmensaje aquí", key = "user_input", on_change = comunicate)
+
+  if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):
+        if isinstance(message, dict):
+            speaker = "😎" if message["role"] == "user" else "🤖"
+            st.write (speaker + ": " + message["content"])
+        else:
+            st.write("🤖: " + message.content)
